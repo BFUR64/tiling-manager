edaf7e36b0e1278d2872c66d21f52c34c2f4611d
Terrance1120 <iterranceevan@gmail.com>
2026-02-10 10:57:25 +0800

    Added skeleton structure

---
+++ lib/src/test/java/com/teic/tiling/LibTest.java
@@ -0,0 +1,12 @@
+package com.teic.tiling;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class LibTest {
+    @Test
+    public void testMethod() {
+        assertEquals(true, true);
+    }
+}
