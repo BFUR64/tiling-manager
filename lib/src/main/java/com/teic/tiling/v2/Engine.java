303aada4f611010e8d7cd8dc6d3902ab777d9d04
BFUR64 <iterranceevan@gmail.com>
2026-06-11 15:43:29 +0800

    refactor: Updated `Layout` to be local to avoid @Nullable

--- lib/src/main/java/com/teic/tiling/v2/Engine.java
+++ lib/src/main/java/com/teic/tiling/v2/Engine.java
@@ -3,14 +3,12 @@ package com.teic.tiling.v2;
 import com.teic.tiling.v2.layouts.LayoutResult;
 import com.teic.tiling.v2.utils.Node;
 import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
 
 @NullMarked
 public class Engine {
     private final Render render;
     private final Node root;
     private final LayoutManager layoutManager;
-    private @Nullable LayoutResult layoutResult;
 
     public Engine(Render render, Node root) {
         this.render = render;
@@ -20,20 +18,19 @@ public class Engine {
 
     public void start() {
         update();
-        layout();
-        render();
+        LayoutResult result = layout();
+        render(result);
     }
 
     private void update() {
         root.update();
     }
 
-    private void layout() {
-        layoutResult = layoutManager.apply(root);
+    private LayoutResult layout() {
+        return layoutManager.apply(root);
     }
 
-    private void render() {
-        assert layoutResult != null;
-        render.render(layoutResult);
+    private void render(LayoutResult result) {
+        render.render(result);
     }
 }
