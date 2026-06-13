db2a73e908b4df7ebd2046a202adb292ba8c33b6
BFUR64 <iterranceevan@gmail.com>
2026-06-13 11:58:11 +0800

    feat: Introduced `measure()` phase and `INSTANCE`

--- lib/src/main/java/com/teic/tiling/v2/layouts/Layout.java
+++ lib/src/main/java/com/teic/tiling/v2/layouts/Layout.java
@@ -1,13 +1,15 @@
 package com.teic.tiling.v2.layouts;
 
+import com.teic.tiling.v2.interfaces.Container;
 import com.teic.tiling.v2.utils.Geometry;
 import com.teic.tiling.v2.utils.Node;
+import com.teic.tiling.v2.utils.Size;
 import org.jspecify.annotations.NullMarked;
 
 import java.util.Map;
 
 @NullMarked
-@FunctionalInterface
 public interface Layout {
+    Size measure(Container container);
     Map<Node, Geometry> apply (Node node, Geometry parentGeometry);
 }
