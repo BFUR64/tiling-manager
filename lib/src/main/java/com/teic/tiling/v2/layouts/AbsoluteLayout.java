9d4c3a240d97e5a08bb7ac2341abc2acccd297fc
BFUR64 <iterranceevan@gmail.com>
2026-06-13 12:17:29 +0800

    refactor: Updated `AbsoluteLayout` to new API changes

--- lib/src/main/java/com/teic/tiling/v2/layouts/AbsoluteLayout.java
+++ lib/src/main/java/com/teic/tiling/v2/layouts/AbsoluteLayout.java
@@ -3,20 +3,39 @@ package com.teic.tiling.v2.layouts;
 import com.teic.tiling.v2.interfaces.Container;
 import com.teic.tiling.v2.utils.Geometry;
 import com.teic.tiling.v2.utils.Node;
+import com.teic.tiling.v2.utils.Size;
 import org.jspecify.annotations.NullMarked;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @NullMarked
-public class AbsoluteLayout {
-    public static Map<Node, Geometry> apply(Node node, Geometry parentGeometry) {
+public class AbsoluteLayout implements Layout {
+    public static final Layout INSTANCE = new AbsoluteLayout();
+
+    private AbsoluteLayout() {}
+
+    @Override
+    public Size measure(Container container) {
+        int x = 0;
+        int y = 0;
+
+        for (Node child : container.getChildren()) {
+            Size size = child.getDesiredSize();
+
+            x = Math.max(x, size.x());
+            y = Math.max(y, size.y());
+        }
+
+        return Size.of(0, 0);
+    }
+
+    @Override
+    public Map<Node, Geometry> layout(Container parent, Geometry geometry) {
         Map<Node, Geometry> local = new HashMap<>();
 
-        if (node instanceof Container parent) {
-            for (Node child : parent.getChildren()) {
-                local.put(child, Geometry.of(child.getDesiredPosition(), child.getDesiredSize()));
-            }
+        for (Node child : parent.getChildren()) {
+            local.put(child, Geometry.of(child.getDesiredPosition(), child.getDesiredSize()));
         }
 
         return local;
