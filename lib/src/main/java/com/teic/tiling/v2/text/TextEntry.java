f92e2ffe5f8230a77bcc5fe037b0b94135aa19a9
BFUR64 <iterranceevan@gmail.com>
2026-06-13 12:01:26 +0800

    refactor: Simplified `of()` in `TextEntry`

--- lib/src/main/java/com/teic/tiling/v2/text/TextEntry.java
+++ lib/src/main/java/com/teic/tiling/v2/text/TextEntry.java
@@ -5,7 +5,7 @@ import com.teic.tiling.v2.utils.Position;
 import java.util.function.Supplier;
 
 public record TextEntry(Position position, Supplier<?> supplier) {
-    public static TextEntry of(int x, int y, Supplier<?> supplier) {
-        return new TextEntry(Position.of(x, y), supplier);
+    public static TextEntry of(Position position, Supplier<?> supplier) {
+        return new TextEntry(position, supplier);
     }
 }
